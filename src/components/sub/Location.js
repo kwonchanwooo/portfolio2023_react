b307d66b809a2c4042b141c8f4eb2d6c0e2689cf
kwonchanwooo <rlaalfk121@gmail.com>
2023-02-17 17:29:02 +0900

    location 추가

--- src/components/sub/Location.js
+++ src/components/sub/Location.js
@@ -1,10 +1,26 @@
-import React from 'react';
+import { useEffect, useRef } from 'react';
 import Layout from '../commoon/Layout';
+function Location() {
+	const container = useRef(null);
+	const { kakao } = window;
+	const option = {
+		center: new kakao.maps.LatLng(33.450701, 126.570667),
+		level: 3,
+	};
+	const markerPosition = option.center;
+	const marker = new kakao.maps.Marker({
+		position: markerPosition,
+	});
 
-function Location(props) {
+	useEffect(() => {
+		const map = new kakao.maps.Map(container.current, option);
+
+		marker.setMap(map);
+	}, []);
+
 	return (
-		<Layout name={'Location'}>
-			<p>Location</p>
+		<Layout name='Location'>
+			<div id='map' ref={container}></div>
 		</Layout>
 	);
 }
