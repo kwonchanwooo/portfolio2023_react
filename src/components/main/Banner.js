bc6941a2acfddb98c9ee9898bbb434ffb31d8dcb
kwonchanwooo <rlaalfk121@gmail.com>
2023-02-19 19:46:12 +0900

    main -banner

--- src/components/main/Banner.js
+++ src/components/main/Banner.js
@@ -1,7 +1,24 @@
 import React from 'react';
 
 function Banner(props) {
-	return <div></div>;
+	return (
+		<section id='banner'>
+			<div class='inner'>
+				<div class='wrap'>
+					<h1>
+						Let's work together on
+						<br /> your next projcet
+					</h1>
+					<p>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga necessitatibus, voluptas
+						commodi doloremque iure voluptates cumque itaque delectus saepe, eligendi tenetur vel
+						suscipit assumenda obcaecati.
+					</p>
+					<a href='community.html'>CONTACT</a>
+				</div>
+			</div>
+		</section>
+	);
 }
 
 export default Banner;
