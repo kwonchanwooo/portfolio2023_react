21450e8f25655153386a1a574343f1c3be8a897f
kwonchanwooo <rlaalfk121@gmail.com>
2023-02-19 19:48:50 +0900

    main -faq

--- src/components/main/Faq.js
+++ src/components/main/Faq.js
@@ -1,7 +1,74 @@
 import React from 'react';
 
 function Faq(props) {
-	return <div></div>;
+	return (
+		<section id='faq'>
+			<div class='container'>
+				<h1> FREQEUNTLY ASKED QUESTIONS</h1>
+
+				<div class='question'>
+					<input type='radio' name='acc' id='acc1' />
+					<label for='acc1'>
+						<h2>01</h2>
+						<h3>How do I apply?</h3>
+					</label>
+					<div class='content'>
+						<p>
+							Lorem ipsum dolor sit amet consectetur adipisicing elit. Est harum accusamus dolorem
+							quae vel amet eligendi? Itaque necessitatibus eligendi, soluta debitis asperiores
+							nobis architecto, consequatur beatae, illum nemo ipsum quis? Lorem ipsum dolor sit,
+							amet consectetur adipisicing elit. Voluptatum adipisci blanditiis, voluptas nemo
+							tempora eos laboriosam officiis! Ex itaque repellendus consectetur dicta expedita
+							perferendis ad modi nobis, iusto obcaecati impedit.
+						</p>
+					</div>
+				</div>
+				<div class='question'>
+					<input type='radio' name='acc' id='acc2' />
+					<label for='acc2'>
+						<h2>02</h2>
+						<h3>How do I apply?</h3>
+					</label>
+					<div class='content'>
+						<p>
+							Lorem ipsum dolor sit amet consectetur adipisicing elit. Est harum accusamus dolorem
+							quae vel amet eligendi? Itaque necessitatibus eligendi, soluta debitis asperiores
+							nobis architecto, consequatur beatae, illum nemo ipsum quis? Lorem ipsum dolor sit,
+						</p>
+					</div>
+				</div>
+				<div class='question'>
+					<input type='radio' name='acc' id='acc3' />
+					<label for='acc3'>
+						<h2>03</h2>
+						<h3>How do I apply?</h3>
+					</label>
+					<div class='content'>
+						<p>
+							Lorem ipsum dolor sit amet consectetur adipisicing elit. Est harum accusamus dolorem
+							quae vel amet eligendi? Itaque necessitatibus eligendi, soluta debitis asperiores
+							nobis architecto, consequatur beatae, illum nemo ipsum quis? Lorem ipsum dolor sit,
+							amet consectetur adipisicing elit.Voluptatum adipisci blanditiis, voluptas nemo
+							tempora eos laboriosam officiis! Ex itaque repellendus consectetur dicta expedita
+							perferendis ad modi nobis, iusto obcaecati impedit.Voluptatum adipisci blanditiis,
+							voluptas nemo tempora eos laboriosam officiis! Ex itaque repellendus consectetur dicta
+							expedita perferendis ad modi nobis, iusto obcaecati impedit.
+						</p>
+					</div>
+				</div>
+				<div class='question'>
+					<input type='radio' name='acc' id='acc4' />
+					<label for='acc4'>
+						<h2>04</h2>
+						<h3>How do I apply?</h3>
+					</label>
+					<div class='content'>
+						<p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+					</div>
+				</div>
+			</div>
+		</section>
+	);
 }
 
 export default Faq;
