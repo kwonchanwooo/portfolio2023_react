63e0654f1d5b7e665d4b6fb07e09de7dee5ee117
kwonchanwooo <rlaalfk121@gmail.com>
2023-03-11 18:37:39 +0900

    컴포너트 이동

--- src/components/sub/About.js
+++ src/components/sub/About.js
@@ -1,7 +1,5 @@
-import { faBus, faParking, faSubway } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import axios from 'axios';
-import { useEffect, useMemo, useRef, useState } from 'react';
+import { useEffect, useState } from 'react';
 import Layout from '../commoon/Layout';
 
 function About(props) {
@@ -12,57 +10,6 @@ function About(props) {
 		});
 	}, []);
 	useEffect(() => {}, [Member]);
-
-	const option = useRef(null);
-	const info = useRef(null);
-	const container = useRef(null);
-	const { kakao } = window;
-	info.current = [
-		{
-			title: '서울 이태원점',
-			latlng: new kakao.maps.LatLng(37.5347906, 126.9935937),
-		},
-		{
-			title: '대구 수성못점',
-			latlng: new kakao.maps.LatLng(35.8252327, 128.6203849),
-		},
-	];
-
-	const [index, setIndex] = useState(0);
-
-	option.current = {
-		center: info.current[index].latlng,
-		level: 3,
-	};
-	const markerPosition = option.current.center;
-	const mapTypeControl = useMemo(() => new kakao.maps.MapTypeControl(), [kakao]);
-	const zoomControl = useMemo(() => new kakao.maps.ZoomControl(), [kakao]);
-
-	const marker = useMemo(() => {
-		return new kakao.maps.Marker({
-			position: markerPosition,
-		});
-	}, [kakao, markerPosition]);
-
-	useEffect(() => {
-		container.current.innerHTML = '';
-		const map = new kakao.maps.Map(container.current, option.current);
-		marker.setMap(map);
-
-		map.addControl(mapTypeControl, kakao.maps.ControlPosition.TOPRIGHT);
-		map.addControl(zoomControl, kakao.maps.ControlPosition.RIGHT);
-
-		const setCenter = () => {
-			map.setCenter(info.current[index].latlng);
-		};
-
-		map.setZoomable(false);
-		window.addEventListener('resize', setCenter);
-
-		return () => {
-			window.removeEventListener('resize', setCenter);
-		};
-	}, [index, kakao, mapTypeControl, marker, zoomControl]);
 
 	return (
 		<Layout name={'About'}>
@@ -93,81 +40,6 @@ function About(props) {
 					</article>
 				);
 			})}
-			<div className='Location'>
-				<h1>Location</h1>
-				<p>
-					Hello. This page introduces the hotel location. The hotel is situated in a convenient
-					location, with many attractions and shopping malls nearby. We also introduce you to the
-					transportation options that you can easily access from the hotel. If you want to know more
-					about the location, please check out the map below.
-				</p>
-				<nav>
-					<ul className='branch'>
-						{info.current.map((el, idx) => {
-							return (
-								<li
-									key={idx}
-									className={idx === index ? 'on' : ''}
-									onClick={() => {
-										setIndex(idx);
-									}}
-								>
-									{el.title}
-								</li>
-							);
-						})}
-					</ul>
-				</nav>
-				<div id='map' ref={container}></div>
-				<div className='way'>
-					{index === 0 ? (
-						<p>서울특별시 용산구 이태원1동 이태원로 179</p>
-					) : (
-						<p>대구광역시 수성구 용학로 106-7</p>
-					)}
-					<div className='bus'>
-						<div className='img'>
-							<FontAwesomeIcon icon={faBus} />
-						</div>
-						{index === 0 ? (
-							<p>
-								<ul>
-									<li>해밀튼호텔 정류장 - 간선 110A, 110B, 421</li>
-									<li>보광동입구 정류장 - 간선 400, 405, 421</li>
-									<li>이태원역앞 정류장 - 용산01</li>
-								</ul>
-							</p>
-						) : (
-							<p>
-								<ul>
-									<li>수성못 건너역 정류장 - 401, 814</li>
-									<li>불교한방병원 정류장 - 410</li>
-								</ul>
-							</p>
-						)}
-					</div>
-					<div className='train'>
-						<div className='img'>
-							<FontAwesomeIcon icon={faSubway} />
-						</div>
-						{index === 0 ? (
-							<p>6호선 이태원역 1, 2번 출구 도보 1분</p>
-						) : (
-							<p>3호선 수성못(TBC)역 2번출구 도보 15분 </p>
-						)}
-					</div>
-					<div className='car'>
-						<div className='img'>
-							<FontAwesomeIcon icon={faParking} />
-						</div>
-						{index === 0 ? (
-							<p>객실 투숙객은 객실당 차량 1대만 무료로 이용 가능</p>
-						) : (
-							<p>객실 투숙객은 객실당 차량 1대만 무료로 이용 가능</p>
-						)}
-					</div>
-				</div>
-			</div>
 		</Layout>
 	);
 }
--- src/components/sub/Community.js
+++ src/components/sub/Community.js
@@ -1,6 +1,7 @@
-import React, { useEffect, useRef, useState } from 'react';
+import { faLocationDot } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React, { useEffect, useMemo, useRef, useState } from 'react';
 import Layout from '../commoon/Layout';
-
 function Community() {
 	const getLocalData = () => {
 		const data = localStorage.getItem('post');
@@ -78,17 +79,167 @@ function Community() {
 		localStorage.setItem('post', JSON.stringify(Posts));
 	}, [Posts]);
 
+	const option = useRef(null);
+	const info = useRef(null);
+	const container = useRef(null);
+	const { kakao } = window;
+	info.current = [
+		{
+			title: '서울 이태원점',
+			latlng: new kakao.maps.LatLng(37.5347906, 126.9935937),
+			imgUrl: `${process.env.PUBLIC_URL}/img/marker.png`,
+			imgSize: new kakao.maps.Size(255, 107),
+			imgPos: { offset: new kakao.maps.Point(120, 100) },
+		},
+		{
+			title: '대구 수성못점',
+			latlng: new kakao.maps.LatLng(35.8252327, 128.6203849),
+			imgUrl: `${process.env.PUBLIC_URL}/img/marker.png`,
+			imgSize: new kakao.maps.Size(255, 107),
+			imgPos: { offset: new kakao.maps.Point(120, 100) },
+		},
+	];
+
+	const [index, setIndex] = useState(0);
+
+	option.current = {
+		center: info.current[index].latlng,
+		level: 3,
+	};
+	const imageSrc = info.current[index].imgUrl;
+	const imageSize = info.current[index].imgSize;
+	const imageOption = info.current[index].imgPos;
+	const markerPosition = option.current.center;
+
+	const markerImage = useMemo(
+		() => new kakao.maps.MarkerImage(imageSrc, imageSize, imageOption),
+		[kakao, imageSrc, imageSize, imageOption]
+	);
+
+	const mapTypeControl = useMemo(() => new kakao.maps.MapTypeControl(), [kakao]);
+	const zoomControl = useMemo(() => new kakao.maps.ZoomControl(), [kakao]);
+
+	const marker = useMemo(() => {
+		return new kakao.maps.Marker({
+			position: markerPosition,
+			image: markerImage,
+		});
+	}, [kakao, markerPosition, markerImage]);
+
+	useEffect(() => {
+		container.current.innerHTML = '';
+		const map = new kakao.maps.Map(container.current, option.current);
+		marker.setMap(map);
+
+		map.addControl(mapTypeControl, kakao.maps.ControlPosition.TOPRIGHT);
+		map.addControl(zoomControl, kakao.maps.ControlPosition.RIGHT);
+
+		const setCenter = () => {
+			map.setCenter(info.current[index].latlng);
+		};
+
+		map.setZoomable(false);
+		window.addEventListener('resize', setCenter);
+
+		return () => {
+			window.removeEventListener('resize', setCenter);
+		};
+	}, [index, kakao, mapTypeControl, marker, zoomControl]);
+
 	return (
 		<Layout name={'Community'}>
-			<div className='inputBox'>
-				<input type='text' placeholder='제목을 입력하세요' ref={input} />
-				<br />
-				<textarea cols='30' rows='3' placeholder='본문을 입력하세요.' ref={textarea}></textarea>
-				<br />
+			<div className='Location'>
+				<h1>Location</h1>
+				<p>
+					Hello. This page introduces the hotel location. The hotel is situated in a convenient
+					location, with many attractions and shopping malls nearby. We also introduce you to the
+					transportation options that you can easily access from the hotel. If you want to know more
+					about the location, please check out the map below.
+				</p>
 
-				<div className='btnSet'>
-					<button onClick={resetForm}>CANCEL</button>
-					<button onClick={createPost}>WRITE</button>
+				<div id='map' ref={container}></div>
+				<nav>
+					<ul className='branch'>
+						{info.current.map((el, idx) => {
+							return (
+								<li
+									key={idx}
+									className={idx === index ? 'on' : ''}
+									onClick={() => {
+										setIndex(idx);
+									}}
+								>
+									{el.title}
+								</li>
+							);
+						})}
+					</ul>
+				</nav>
+			</div>
+			<div className='box'>
+				<div className='inputBox'>
+					<input type='text' placeholder='제목을 입력하세요' ref={input} />
+					<br />
+					<textarea cols='30' rows='3' placeholder='본문을 입력하세요.' ref={textarea}></textarea>
+					<br />
+
+					<div className='btnSet'>
+						<button onClick={resetForm}>CANCEL</button>
+						<button onClick={createPost}>WRITE</button>
+					</div>
+				</div>
+				<div className='way'>
+					{index === 0 ? <h2>서울 이태원점</h2> : <h2>대구 수성못점</h2>}
+					<div className='adr'>
+						<FontAwesomeIcon icon={faLocationDot} />
+						{index === 0 ? (
+							<p> 서울특별시 용산구 이태원1동 이태원로 179</p>
+						) : (
+							<p>대구광역시 수성구 용학로 106-7</p>
+						)}
+					</div>
+
+					{/* <div className='bus'>
+						<div className='img'>
+							<FontAwesomeIcon icon={faBus} />
+						</div>
+						{index === 0 ? (
+							<p>
+								<ul>
+									<li>해밀튼호텔 정류장 - 간선 110A, 110B, 421</li>
+									<li>보광동입구 정류장 - 간선 400, 405, 421</li>
+									<li>이태원역앞 정류장 - 용산01</li>
+								</ul>
+							</p>
+						) : (
+							<p>
+								<ul>
+									<li>수성못 건너역 정류장 - 401, 814</li>
+									<li>불교한방병원 정류장 - 410</li>
+								</ul>
+							</p>
+						)}
+					</div>
+					<div className='train'>
+						<div className='img'>
+							<FontAwesomeIcon icon={faSubway} />
+						</div>
+						{index === 0 ? (
+							<p>6호선 이태원역 1, 2번 출구 도보 1분</p>
+						) : (
+							<p>3호선 수성못(TBC)역 2번출구 도보 15분 </p>
+						)}
+					</div>
+					<div className='car'>
+						<div className='img'>
+							<FontAwesomeIcon icon={faParking} />
+						</div>
+						{index === 0 ? (
+							<p>객실 투숙객은 객실당 차량 1대만 무료로 이용 가능</p>
+						) : (
+							<p>객실 투숙객은 객실당 차량 1대만 무료로 이용 가능</p>
+						)}
+					</div> */}
 				</div>
 			</div>
 
